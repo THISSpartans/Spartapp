dbbb3c19900259467af765aa34ced42802932c83
Alan Tao <alantao20011015@outlook.com>
2018-08-11 14:00:55 +0800

    -added default channel-finding method

--- app/src/main/java/hackthis/team/spartapp/Club.java
+++ app/src/main/java/hackthis/team/spartapp/Club.java
@@ -3,11 +3,24 @@ package hackthis.team.spartapp;
 public class Club {
     String name;
     boolean checked;
+    private String channel = null;
 
     Club(String n){name = n; checked = false;}
 
     public String getCorrespondingChannel(){
-        return name;
+        char[] str = name.toCharArray();
+        if(channel == null) {
+            StringBuilder sb = new StringBuilder();
+            for (char i : str) {
+                if (((int) i) <= ((int) 'z') && ((int) i) >= ((int) 'a')) {
+                    sb.append(i);
+                } else if (((int) i) <= ((int) 'Z') && ((int) i) >= ((int) 'A')) {
+                    sb.append(i);
+                }
+            }
+            channel = sb.toString();
+        }
+        return channel;
     }
 
     @Override
