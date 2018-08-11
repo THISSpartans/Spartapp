794e7600bb0abd452f4e31d65bb768b7d450fe25
Alan Tao <alantao20011015@outlook.com>
2018-08-11 14:00:32 +0800

    -added push service

--- app/src/main/java/hackthis/team/spartapp/MyLeanCloudApp.java
+++ app/src/main/java/hackthis/team/spartapp/MyLeanCloudApp.java
@@ -2,16 +2,37 @@ package hackthis.team.spartapp;
 
 import android.app.Application;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVInstallation;
 import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.PushService;
+import com.avos.avoscloud.SaveCallback;
 
 public class MyLeanCloudApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        PushService.setDefaultChannelId(this, "Spartapp");
+
         // 初始化参数依次为 this, AppId, AppKey
         AVOSCloud.initialize(this,"cRxhzMwEQJ07JfRuztRYFJ5n-gzGzoHsz","kIvYOVL1hGnkS3n1kh76P8NC");
         //TODO: remove before release
         AVOSCloud.setDebugLogEnabled(true);
+
+        AVInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
+            public void done(AVException e) {
+                if (e == null) {
+                    // 保存成功
+                    String installationId = AVInstallation.getCurrentInstallation().getInstallationId();
+                    // 关联  installationId 到用户表等操作……
+                } else {
+                    // 保存失败，输出错误信息
+                }
+            }
+        });
+
+        // 设置默认打开的 Activity
+        PushService.setDefaultPushCallback(this, MainActivity.class);
     }
 }
