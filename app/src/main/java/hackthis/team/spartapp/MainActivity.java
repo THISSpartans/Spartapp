4fdd5f1cbf62566adc3e9893f92ffe9be7392b10
Alan Tao <alantao20011015@outlook.com>
2018-12-24 19:39:12 +0800

    -download feature -added school channels

--- app/src/main/java/hackthis/team/spartapp/MainActivity.java
+++ app/src/main/java/hackthis/team/spartapp/MainActivity.java
@@ -1,21 +1,31 @@
 package hackthis.team.spartapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,15 +35,17 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVInstallation;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.GetCallback;
+import com.avos.avoscloud.PushService;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
@@ -44,6 +56,8 @@ public class MainActivity extends Activity {
     FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
     SharedPreferences sp;
+
+    final int RequestCode = 114514;
 
     //switch between activities
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -97,12 +111,21 @@ public class MainActivity extends Activity {
             //sp.edit().putInt("grade",grade).apply();
             RadioButton THIS = (RadioButton) findViewById(R.id.radio_this);
             //RadioButton ISB = (RadioButton) findViewById(R.id.radio_isb);
+
+
+
             if(THIS.isChecked()){
                 sp.edit().putString("school","THIS").apply();
+                PushService.subscribe(MainActivity.this,"tsinghua",MainActivity.class);
+                PushService.unsubscribe(MainActivity.this,"isb");
             }
             else{
                 sp.edit().putString("school","ISB").apply();
+                PushService.subscribe(MainActivity.this,"isb",MainActivity.class);
+                PushService.unsubscribe(MainActivity.this,"tsinghua");
             }
+
+            AVInstallation.getCurrentInstallation().saveInBackground();
 
             RadioButton STU = (RadioButton) findViewById(R.id.radio_student);
             //RadioButton TEA = (RadioButton) findViewById(R.id.radio_teacher);
@@ -119,10 +142,51 @@ public class MainActivity extends Activity {
         }
     };
 
+    private BroadcastReceiver ApkInstallReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() != null && intent.getAction().equals(DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
+                //https://stackoverflow.com/questions/26210048/how-to-receive-status-of-download-manager-intent-until-download-success-or-faile/38737503#38737503
+                Toast.makeText(context,"downloaded", Toast.LENGTH_SHORT).show();
+                long completeDownloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                installApk(context, completeDownloadId);
+                LogUtil.d("DownloadManager",Long.toString(completeDownloadId));
+            }
+        }
+    };
+
+        /*
+        ---------------------
+        作者：Chiclaim
+        来源：CSDN
+        原文：https://blog.csdn.net/johnny901114/article/details/51472600
+        版权声明：本文为博主原创文章，转载请附上博文链接！*/
+        private void installApk(Context context, long downloadApkId) {
+            DownloadManager dManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            Intent install = new Intent(Intent.ACTION_VIEW);
+            try {
+                Uri downloadFileUri = dManager.getUriForDownloadedFile(downloadApkId);
+                if (downloadFileUri != null) {
+                    LogUtil.d("DownloadManager", downloadFileUri.toString());
+                    install.setDataAndType(downloadFileUri, "application/vnd.android.package-archive");
+                    install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //Toast.makeText(MainActivity.this, "jier",Toast.LENGTH_LONG).show();
+                    context.startActivity(install);
+                } else {
+                    LogUtil.e("DownloadManager", "download error");
+                }
+            }
+            catch(Exception E){
+                E.printStackTrace();
+            }
+        }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
+
+        registerReceiver(ApkInstallReceiver,new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
 
         sp = getSharedPreferences("app", Context.MODE_PRIVATE);
         boolean first = sp.getBoolean("first_launch",true);
@@ -154,9 +218,26 @@ public class MainActivity extends Activity {
         }
 
         else {
+
+            sp = getSharedPreferences("clubs",Context.MODE_PRIVATE);
+            String school = sp.getString("school","none");
+            if(!school.equals("none")){
+                switch(school){
+                    case "THIS":{
+                        PushService.subscribe(MainActivity.this,"tsinghua",MainActivity.class);
+                        PushService.unsubscribe(MainActivity.this,"isb");
+                        break;
+                    }
+                    case "ISB":{
+                        PushService.subscribe(MainActivity.this,"isb",MainActivity.class);
+                        PushService.unsubscribe(MainActivity.this,"tsinghua");
+                        break;
+                    }
+                }
+            }
+            AVInstallation.getCurrentInstallation().saveInBackground();
             init_main();
         }
-
 
         //get access to internet
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
@@ -174,6 +255,7 @@ public class MainActivity extends Activity {
                 if(e==null) {
                     String versionName = avObject.getString("version_name");
                     int versionCode = avObject.getInt("version_code");
+                    String description = avObject.getString("description");
 
                     //dialogue for version name
                     try {
@@ -181,20 +263,18 @@ public class MainActivity extends Activity {
                         int localVersionCode = pInfo.versionCode;
                         LogUtil.d("VER", "local version code is " + localVersionCode);
 
-                        if (localVersionCode < versionCode) {
+                        if (versionCode > localVersionCode) {
                             AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
                             alertBuilder.setPositiveButton("update", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
-                                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(avObject.getString("URL")));
-                                    startActivity(browserIntent);
+                                    download();
                                 }
                             }).setNegativeButton("ignore", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
 
                                 }
-                            }).setMessage("A new version " + versionName + " is available.")
+                            }).setMessage(description)
                                     .setCancelable(true);
-                            LogUtil.d("VER", "connected and obtained version " + versionName + " with code " + versionCode);
                             AlertDialog dialog = alertBuilder.create();
                             dialog.show();
                         }
@@ -207,6 +287,89 @@ public class MainActivity extends Activity {
         });
     }
 
+    //https://stackoverflow.com/questions/21477493/android-download-manager-completed
+    @Override
+    public void onStop() {
+        unregisterReceiver(ApkInstallReceiver);
+        super.onStop();
+    }
+
+
+    //https://stackoverflow.com/questions/40514335/download-manager-android-permissions-error-write-external-storage
+    public  boolean haveStoragePermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                LogUtil.e("Permission error","You have permission");
+                return true;
+            } else {
+
+                LogUtil.e("Permission error","You have asked for permission");
+                return false;
+            }
+        }
+        else { //you dont need to worry about these stuff below api level 23
+            LogUtil.e("Permission error","You already have the permission");
+            return true;
+        }
+    }
+
+    public void download(){
+        if(haveStoragePermission()) {
+            DownloadManager.Request req = new DownloadManager.Request(Uri.parse("https://thisprogrammingclub.github.io/spartapp_android.apk"));
+            req.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "spartapp_update.apk");
+            // 设置一些基本显示信息
+            req.setTitle("Spartapp");
+            req.setDescription("New Version");
+            req.setMimeType("application/vnd.android.package-archive");
+        /*
+        ---------------------
+                作者：Chiclaim
+        来源：CSDN
+        原文：https://blog.csdn.net/johnny901114/article/details/51472600
+        版权声明：本文为博主原创文章，转载请附上博文链接！
+        */
+            req.setVisibleInDownloadsUi(true);
+            req.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+
+            long downloadID = dm.enqueue(req);
+        }
+        else{
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    RequestCode);
+
+
+        }
+    }
+
+    //https://developer.android.com/training/permissions/requesting?hl=zh-cn
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestCode: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    download();
+
+                } else {
+                    startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.spartapp.org")));
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
     public void init_main(){
         boolean schExist = true;
         try {
@@ -215,7 +378,7 @@ public class MainActivity extends Activity {
         catch(Exception e){
             schExist = false;
         }
-        //todo determine if the user already logged in, if true do follows:
+        //determine if the user already logged in, if true do follows:
         if(schExist) {
             setContentView(R.layout.activity_main);
 
@@ -234,7 +397,6 @@ public class MainActivity extends Activity {
         }
         else {
             LogUtil.d("ERR", "IOE");
-            //todo if false, do follows
             Intent login = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(login);
         }
